61630ebd8a12fb3026bedee82e1067b8cb6b5138
Jakub Rożek <jakub@stoplight.io>
2019-04-24 23:35:59 +0200

    feat: EventEmitter#on accepts listener

--- src/emitter/emitter.ts
+++ src/emitter/emitter.ts
@@ -1,32 +1,28 @@
 import Emitter = require('wolfy87-eventemitter');
 
-import { createDisposable } from '../disposable';
-import { EventEmitterOn, IEventEmitter } from './types';
+import { createDisposable, IDisposable } from '../disposable';
+import { IEventEmitter } from './types';
 
-export class EventEmitter<T, N = string> implements IEventEmitter<T, N> {
-  // @ts-ignore the mitt typings are wrong
+export class EventEmitter<E extends object> implements IEventEmitter<E> {
   private _emitter = new Emitter();
 
-  public on(type: N): EventEmitterOn<T> {
-    return listener => {
-      // @ts-ignore
-      this._emitter.on(type, listener);
-      return createDisposable(() => {
-        // @ts-ignore
-        this._emitter.off(type, listener);
-      });
-    };
+  public on(type: unknown, listener: Function): IDisposable {
+    this._emitter.on(String(type), listener);
+    return createDisposable(() => {
+      this._emitter.off(String(type), listener);
+    });
   }
 
-  public emit(type: N, e: T): void {
-    // @ts-ignore
-    this._emitter.trigger(type, [e]);
+  public emit(type: unknown, ...args: Array<unknown>): void {
+    this._emitter.trigger(String(type), args);
   }
 
   public get hasListeners() {
     const eventsToListeners = this._emitter.getListeners(/.*/);
 
     for (const ev in eventsToListeners) {
+      if (!{}.hasOwnProperty.call(eventsToListeners, ev)) continue;
+
       const l = eventsToListeners[ev];
       if (l !== undefined && l.length > 0) {
         return true;
